b3124f50f4d57b9b7901d0fe8265c3ea3b4201ad
lorenzo <lorenzo@lollosan>
2017-04-01 16:21:24 +0200

    mappa funzionante

---
+++ JavaApplication3/data/grass.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass" tilewidth="52" tileheight="32" tilecount="8" columns="2">
+ <properties>
+  <property name="blocked" value="false"/>
+ </properties>
+ <image source="grass.png" width="128" height="128"/>
+</tileset>
