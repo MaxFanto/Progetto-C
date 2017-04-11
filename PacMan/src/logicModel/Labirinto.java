2b29bbd4aa35da6b4e56399d5630ab00fb6dc2ec
Mattia <mattia.rogledi@gmail.com>
2017-04-11 17:02:35 +0200

    aggiunta classe labirinto

--- PacMan/src/logicModel/Labirinto.java
+++ PacMan/src/logicModel/Labirinto.java
@@ -5,10 +5,39 @@
  */
 package logicModel;
 
+import altro.Frutto;
+import java.util.Random;
+
 /**
  *
  * @author lorenzo
  */
 public class Labirinto {
+    private int larghezza, altezza, numTiles;
     
+    
+    
+    
+    public Frutto generaFrutto(){
+        Random random = new Random();
+        int indiceFrutto = random.nextInt(6);
+        
+        switch(indiceFrutto){
+            case 0:
+                return Frutto.CILIEGIA;               
+            case 1:
+                return Frutto.FRAGOLA;               
+            case 2:
+                return Frutto.ARANCIA;              
+            case 3:
+                return Frutto.MELA;            
+            case 4:
+                return Frutto.UVA;        
+            case 5:
+                return Frutto.CAMPANA;
+            case 6:
+                return Frutto.CHIAVE;                
+        }
+        return null;
+    }
 }
