65021966d15b417ee0edc001b9d6d7f417290378
Mattia <mattia.rogledi@gmail.com>
2017-04-11 17:05:28 +0200

    aggiunta della classe labirinto

---
+++ PacMan/src/altro/Frutto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altro;
+
+/**
+ *
+ * @author cl427927
+ */
+public enum Frutto {
+    CILIEGIA,FRAGOLA,ARANCIA,MELA,UVA,CAMPANA,CHIAVE
+}
