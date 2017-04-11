21512f8f44fd009005f2875bbc62f9993630c7d2
Mattia <mattia.rogledi@gmail.com>
2017-04-11 09:50:47 +0200

    aggiunta fantasmini

---
+++ PacMan/src/logicModel/Akabei.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicModel;
+
+/**
+ *
+ * @author cl427927
+ * 
+ */
+public class Akabei extends Fantasma{
+    /*
+        RED
+    */
+    public Akabei(double xPos, double yPos) {
+        super(xPos, yPos);
+    }
+    
+    @Override
+    public void movimento(double xPacMan, double yPacMan) {
+        double myXPos = super.getxPos();
+        double myYPos = super.getyPos();
+        
+        
+    }
+    
+}
