74af77a07470bfcf7835dc45b4490d789b6cb8fd
cl426899 <cl426899@localhost>
2017-05-23 11:56:49 +0200

    pulizia e sistemazione guzuta

--- PacMan/src/model/Fantasmi/Guzuta.java
+++ PacMan/src/model/Fantasmi/Guzuta.java
@@ -20,52 +20,37 @@ public class Guzuta extends Fantasma {
     public Guzuta(int tile_width, int tile_heigth, int mapWidth, Tile[][] tiles) {
         super(tile_width, tile_heigth, mapWidth, tiles);
         currentDirection = choose_direction();
-        counter = 15;
+        //counter = 15;
         
     }
 
     public int choose_direction() {
-                        System.out.println(c);
-
-        setCorners();
-        int a = 0;
-        int b = 3;
-        precedente = randNumber;
-        
-        
+        setCorners();        
         boolean validDirection = false;
         Random rand = new Random();
-        if(counter == 15 ){
-            do{
-                switch(randNumber){
-                    case 0:
-                        if((validDirection=controlloBlockedSu()) == false)
-                            randNumber = rand.nextInt(2)+2;
-                        break;
-                    case 1:
-                        if((validDirection=controlloBlockedGiu()) == false)
-                            randNumber = rand.nextInt(2)+2;
-                        break;
-                    case 2:
-                        if((validDirection=controlloBlockedSx()) == false){
-                            randNumber = rand.nextInt(2);
-                        }
-                        break;
-                    case 3:
-                        if((validDirection=controlloBlockedDx()) == false){
-                            randNumber = rand.nextInt(2);
-                        }
-                        break;
+        
+        switch(randNumber){
+            case 0:
+                if((validDirection=controlloBlockedSu()) == false)
+                    randNumber = rand.nextInt(2)+2;
+                break;
+            case 1:
+                if((validDirection=controlloBlockedGiu()) == false)
+                    randNumber = rand.nextInt(2)+2;
+                break;
+            case 2:
+                if((validDirection=controlloBlockedSx()) == false){
+                    randNumber = rand.nextInt(2);
                 }
-            }while(validDirection == false);
-            currentDirection = randNumber;
-            counter = 0;
-        } else{
-            counter++;
-            return randNumber = currentDirection;
+                break;
+            case 3:
+                if((validDirection=controlloBlockedDx()) == false){
+                    randNumber = rand.nextInt(2);
+                }
+                break;
         }
-
-            return randNumber;
+        
+        return randNumber;
     }
     
     
