066d5a45d38d1704f339dbe40fda9196baf98f1f
Lollodan <Lollodan@10.87.224.68>
2017-05-24 15:53:14 +0200

    fix vari2

--- PacMan/src/model/Fantasmi/Clyde.java
+++ PacMan/src/model/Fantasmi/Clyde.java
@@ -19,6 +19,8 @@ public class Clyde extends Fantasma {
     int precedente, c;
     private int X_MAIN_POS = 224;
     private int Y_MAIN_POS = 256;
+    Random rand = new Random();
+
     
     public Clyde(int tile_width, int tile_heigth, int mapWidth, Tile[][] tiles) {
         super(tile_width, tile_heigth, mapWidth, tiles);
@@ -28,32 +30,35 @@ public class Clyde extends Fantasma {
     }
 
     public int choose_direction() {
-        setCorners();        
-        boolean validDirection = false;
-        Random rand = new Random();
+        setCorners();                
+        direction();
+        return randNumber;
+    }
+
+    private void direction() {
         
+        precedente = randNumber;
+
         switch(randNumber){
             case 0:
-                if((validDirection=controlloBlockedSu()) == false)
+                if((controlloBlockedSu()) == false)
                     randNumber = rand.nextInt(2)+2;
                 break;
             case 1:
-                if((validDirection=controlloBlockedGiu()) == false)
+                if((controlloBlockedGiu()) == false)
                     randNumber = rand.nextInt(2)+2;
                 break;
             case 2:
-                if((validDirection=controlloBlockedSx()) == false){
+                if((controlloBlockedSx()) == false){
                     randNumber = rand.nextInt(2);
                 }
                 break;
             case 3:
-                if((validDirection=controlloBlockedDx()) == false){
+                if((controlloBlockedDx()) == false){
                     randNumber = rand.nextInt(2);
                 }
                 break;
         }
-        
-        return randNumber;
     }
     
     
