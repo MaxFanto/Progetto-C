2d3614cb4cf629e9adc0d7c35d7970ed1eb0290e
matteo <matteo@matteo-PC-pavHP>
2017-07-12 10:43:04 +0200

    tolto buil-jar

--- PacMan/src/test/TestLabirinto.java
+++ PacMan/src/test/TestLabirinto.java
@@ -9,16 +9,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import view.VistaLabirinto;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author cl427927
- */
 public class TestLabirinto {
     public static void main(String[] args) throws SlickException, InterruptedException {
         
